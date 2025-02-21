5a2b63bdc598ef535be9662bf8b8d38695855d09
Avery <2831956988@qq.com>
2025-02-21 14:28:23 +0800

    Exercise #3: Java Basics-Part 1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Slice_o_Heaven slice_o_heaven = new Slice_o_Heaven();
+        slice_o_heaven.otakeorder();
+        slice_o_heaven.makePizza("mashroom",13.2,6,"null","1234",13.2);
     }
 }
---
+++ src/Slice_o_Heaven.java
@@ -0,0 +1,53 @@
+public class Slice_o_Heaven {
+    public String ostoreName;
+    public String storeAddress;
+    public String storeEmail;
+    public long storePhone;
+    public String storeMenu;
+
+    private String Pizzalngredients;
+    private double pizzaPrice;
+    private int size;
+    private String drinks;
+    private String orderID;
+    private double orderTotle;
+
+    public void otakeorder(){
+        
+        System.out.println("Order is accepted");
+        System.out.println("Order is being prepared");
+
+
+        
+    }
+
+    public void makePizza(String lng,double Price,int si, String dr, String ID,double Totle){
+        Pizzalngredients=lng;
+        pizzaPrice=Price;
+        size=si;
+        drinks=dr;
+        orderID=ID;
+        orderTotle=Totle;
+        try{
+            Thread.sleep(5000);
+        }catch(InterruptedException e){
+            System.out.println("for pickup");
+        }
+
+        System.out.println("ready");
+        printReceipt();
+
+    }
+
+    private void printReceipt(){
+        System.out.println("*************Receipt*****************");
+        System.out.println("ID "+orderID);
+        System.out.println("size "+size);
+        System.out.println("Pizzalngredients "+Pizzalngredients);
+        System.out.println("drinks "+drinks);
+        System.out.println("Totle "+orderTotle);
+
+
+
+    }
+}
